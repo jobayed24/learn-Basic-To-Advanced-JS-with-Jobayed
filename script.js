3c9c4bb8caab3367815636d43c417112534ba273
jobayed24 <jkhouse117@gmail.com>
2024-01-31 23:26:32 +0600

    same list show using array foreach method

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 function printList(){
     let text="<ul>";
     let getDemo=document.querySelector("#demo");
-
-    
-    for(let i=0;i<fruits.length; i++){
-        text+="<li>"+fruits[i]+"</li>";
-    }
+    fruits.forEach((ele)=>{
+       return text+="<li>"+ele+"</li>"
+    })
     text+="</ul>"
     
     getDemo.innerHTML=text;
