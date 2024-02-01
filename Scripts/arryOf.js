70a96e6d518627d46a6c18f22643f2888610453a
jobayed24 <jkhouse117@gmail.com>
2024-02-02 00:24:12 +0600

    arrayof method takes argument and create new array on the other hand arrayfrom crate shallow copy of orignal array

--- Scripts/arryOf.js
+++ Scripts/arryOf.js
@@ -1,3 +1,9 @@
-let fruits = Array.of("Banana", "Orange", "Apple", "Mango");
+let fruits=["Banana", "Orange", "Apple", "Mango","Graps"];
+/// array.from method take two paramenter one is array secondis mapfn where mapfn is travers every element of array 
 
-console.log(fruits)
+//if i need to add something new with array element then we can use it and it return new array 
+
+// which is shallow copy it's mean it takes referenc of main array
+let getFromFruits=Array.from(fruits,(x)=>x+"jk");
+
+console.log(getFromFruits)
