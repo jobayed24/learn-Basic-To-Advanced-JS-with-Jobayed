ffc233d0551dc499b51a5f3d29aab4530f6eba17
jobayed24 <jkhouse117@gmail.com>
2024-02-02 00:28:59 +0600

    adding jk with array element with map

--- Scripts/arryOf.js
+++ Scripts/arryOf.js
@@ -1,9 +1,5 @@
 let fruits=["Banana", "Orange", "Apple", "Mango","Graps"];
-/// array.from method take two paramenter one is array secondis mapfn where mapfn is travers every element of array 
-
-//if i need to add something new with array element then we can use it and it return new array 
-
-// which is shallow copy it's mean it takes referenc of main array
-let getFromFruits=Array.from(fruits,(x)=>x+"jk");
+// we can achived same thing using map function
+let getFromFruits=fruits.map((ele)=>ele+"jk")
 
 console.log(getFromFruits)
