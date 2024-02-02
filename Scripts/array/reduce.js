6d45286f95859d360ba6f3b937401d52768b28b7
jobayed24 <jkhouse117@gmail.com>
2024-02-02 20:58:54 +0600

    reduce and reduceRight are same

--- Scripts/array/reduce.js
+++ Scripts/array/reduce.js
@@ -1,9 +1,11 @@
 const numbers = [2, 2, 2, 2, 2];
 
-const reduceNumbers=numbers.reduce(functionNumbers,100);
+// const reduceNumbers=numbers.reduce(functionNumbers,100);
+// array reduce and reduceRight are almost same work
+const reduceNumber=numbers.reduceRight(functionNumbers,210)
 
 function functionNumbers(total,value,index,arr){
     return total+=value;
 }
 
-console.log(reduceNumbers)
+console.log(reduceNumber)
