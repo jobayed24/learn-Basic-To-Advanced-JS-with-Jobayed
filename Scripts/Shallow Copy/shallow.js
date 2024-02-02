76ebc23ab62fc4941ffac0d50adafb841037fae9
jobayed24 <jkhouse117@gmail.com>
2024-02-02 15:31:24 +0600

    object deep copy using structured clone

--- Scripts/Shallow Copy/shallow.js
+++ Scripts/Shallow Copy/shallow.js
@@ -1,8 +1,8 @@
 const a = { x: 0, y: { z: 0 } };
 
 //deep copy
-const deepCopyA=JSON.parse(JSON.stringify(a))
+const deepCopyA=structuredClone(a)
 
 deepCopyA.y.z="10";
 
-console.log(deepCopyA.y.z)
+console.log(a.y.z)
