be1545149bb9d41814ddee3d79b24cab70ac249e
jobayed24 <jkhouse117@gmail.com>
2024-03-29 15:06:30 +0600

    mouse move feature added and get clienx and client y

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-let myInputField=document.getElementById("myInputField");
 
-function myOnMouseEneterFn(){
-   myInputField.style.display="inline-block"
-   myInputField.style.padding="5px 10px"
-   myInputField.style.marginTop="10px";
-   myInputField.style.borderRadius="3px"
-
-   myInputField.style.backgroundColor="green"
+function myOnMouseOut(){
+    document.getElementById('demo').innerText=""
 }
 
-function myOnMouseLeaveFn(){
-     myInputField.style.display="block"
-   myInputField.style.padding=" "
-   myInputField.style.marginTop=" ";
-   myInputField.style.borderRadius=""
-
-   myInputField.style.backgroundColor=""
+function myOnMouseMove(e){
+    
+    let x=e.clientX;
+    let y=e.clientY;
+  
+   
+    let color="Coordinate: "+x+" "+y;
+    document.getElementById("demo").innerText=color
 }
