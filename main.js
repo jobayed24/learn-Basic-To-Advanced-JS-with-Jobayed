5dc8cc8d9e28773df335d5bd4059c7022feef6a6
jobayed24 <jkhouse117@gmail.com>
2024-03-29 14:49:23 +0600

    onMouseEneter and onMouseLeave functionality added

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 let myInputField=document.getElementById("myInputField");
 
-function myOnDbClickFn(){
-   document.getElementById("demo").innerHTML="YOu make duble clicked"
+function myOnMouseEneterFn(){
+   myInputField.style.display="inline-block"
+   myInputField.style.padding="5px 10px"
+   myInputField.style.marginTop="10px";
+   myInputField.style.borderRadius="3px"
+
+   myInputField.style.backgroundColor="green"
+}
+
+function myOnMouseLeaveFn(){
+     myInputField.style.display="block"
+   myInputField.style.padding=" "
+   myInputField.style.marginTop=" ";
+   myInputField.style.borderRadius=""
+
+   myInputField.style.backgroundColor=""
 }
