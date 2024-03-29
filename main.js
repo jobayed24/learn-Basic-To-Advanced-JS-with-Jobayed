683ca5108eaf796d13eb2e22f6e64741e844fda7
jobayed24 <jkhouse117@gmail.com>
2024-03-29 14:34:25 +0600

    db click feature added

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 let myInputField=document.getElementById("myInputField");
 
-function myOnCopyFn(){
-    document.getElementById("demo").innerHTML="<span>"+myInputField.value+"</span>"
-    function clearDemoDiv(){
-        document.getElementById("demo").innerHTML=""
-    }
-
-    setTimeout(clearDemoDiv,3000)
+function myOnDbClickFn(){
+   document.getElementById("demo").innerHTML="YOu make duble clicked"
 }
