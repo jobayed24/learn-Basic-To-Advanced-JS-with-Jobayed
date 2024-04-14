442fe2ac633a100f19c3a9b327da9b284582c764
jobayed24 <jkhouse117@gmail.com>
2024-04-14 20:29:21 +0600

    String.endWith return trure or false as given string

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-const str = "The quick red fox jumped over the lazy dog's back";
+const str1 = 'Cats are the best!';
 
-
-
-function lastStrAt(str){
-    return str.at(-1)
-}
-function lastStrCharat(str){
-    return str.charAt(str.length-1)
-}
-function spliceChar(str,start,end){
-    let strValue= str.slice(start,end)
-    console.log(strValue.toUpperCase())
-}
-
-console.log('lastStrAt',lastStrAt(str))
-console.log('laststrChartat',lastStrCharat(str))
-
-spliceChar(str,-4)
+console.log(str1.endsWith('best', (str1.length-1)));
