c493f2f5ece89e550dc02b9e62d410996728a97b
jobayed24 <jkhouse117@gmail.com>
2024-04-27 10:29:33 +0600

    filtering data and show data in UI

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const user=['jobayed','jonayed','j','tanvir','taju','shakhwat','rasel','raju','riaz'];
+const user=['jobayed','jonayed','j','tanvir','taju','shakhwat','rasel','raju','riaz','aklima','tushar','tonni','rejbi','honufa','jakir','ohid','rabaya'];
 
 function onFilter(event){
-    
+    console.log(event)
   let filterVlaue=  user.filter((value)=>value.includes(event.target.value))
     if(filterVlaue.length!==user.length){
     document.getElementById('showData').innerHTML=filterVlaue;
     document.getElementById('lengthShow').innerHTML=filterVlaue.length+" Matches";
     }else{
-        document.getElementById('showData').innerHTML=''
-        document.getElementById('lengthShow').innerHTML=''
+        document.getElementById('showData').innerHTML=null
+        document.getElementById('lengthShow').innerHTML=null
     }
 }
