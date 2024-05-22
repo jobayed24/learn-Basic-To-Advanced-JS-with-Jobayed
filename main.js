b5e538de4234e81b3f09b38dcf8e3ecfa111481b
jobayed24 <jkhouse117@gmail.com>
2024-05-22 08:04:40 +0600

    promis return objectt with given callback function

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-const user=['jobayed','jonayed','j','tanvir','taju','shakhwat','rasel','raju','riaz','aklima','tushar','tonni','rejbi','honufa','jakir','ohid','rabaya'];
 
-function onFilter(event){
-    console.log(event)
-  let filterVlaue=  user.filter((value)=>value.includes(event.target.value))
-    if(filterVlaue.length!==user.length){
-    document.getElementById('showData').innerHTML=filterVlaue;
-    document.getElementById('lengthShow').innerHTML=filterVlaue.length+" Matches";
-    }else{
-        document.getElementById('showData').innerHTML=null
-        document.getElementById('lengthShow').innerHTML=null
-    }
+function seccessFullCallback(result){
+  console.log(`audio file ready at url: ${result}`)
 }
+function failurCallback(error){
+  console.error(`error generating audio file: ${error}`)
+}
+///callback system
+createAudioFileAsync(audiSetting,seccessFullCallback,failurCallback)
+/// promis and asynchronous behavior
+
+createAudioFileAsync(audioSetting).then(successfullCallback,failurCallback)
