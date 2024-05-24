4370a2a98a1c50869e9aa5f7024057cc2b4eb0ad
jobayed24 <jkhouse117@gmail.com>
2024-05-24 17:59:46 +0600

    handle sepertely promise all by useing handle error

--- Promis/promise.js
+++ Promis/promise.js
@@ -1,21 +1,29 @@
-//Promise all
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("one"), 1000);
+  });
+  const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("two"), 2000);
+  });
+  const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("three"), 3000);
+  });
+  const p4 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("four"), 4000);
+  });
+  const p5 = new Promise((resolve, reject) => {
+    reject(new Error("reject"));
+  });
+  
+  // Using .catch:
+  //set error function to handle error 
 
-const promise1=Promise.resolve(3);
+  Promise.all([p1.catch((error)=>error), p2.catch((error)=>error), p3.catch((error)=>error), p4.catch((error)=>error), p5.catch((error)=>error)])
+    .then((values) => {
+      values.forEach((values)=>console.log(values))
 
-const promise3=new Promise((resolve,reject)=>{
-    setTimeout(resolve, 100,'foo');
-})
-const promise2=new Promise((resolve)=> {
-    throw Error('error occured')
-});
-Promise.allSettled([promise1,promise3,promise2,''])
-    .then((values)=>{
-        values.forEach((value)=>{
-            if(value.status=="fulfilled"){
-                console.log('fulfiled value')
-            }else if(value.status=='rejected'){
-                console.log(value.reason.message)
-            }
-        })
     })
    
+  
+  // Logs:
+  // "reject"
+  
