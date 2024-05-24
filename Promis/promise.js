10968bc516dea5487479e89c1ea2bd56265f5b85
jobayed24 <jkhouse117@gmail.com>
2024-05-24 09:31:23 +0600

    promise callback handle as microtask whereas setTimeout handled as task queues

--- Promis/promise.js
+++ Promis/promise.js
@@ -1,11 +1,13 @@
-const wait = (ms) =>new Promise((resolve)=>setTimeout(resolve, ms))
-
+const wait =(ms)=>new Promise((res)=>setTimeout(res, ms))
 
 wait(0)
+    .then(()=>console.log("promise callback"))
+
+    const wait1 =(ms)=>new Promise((res)=>setTimeout(res, ms))
+
+    wait1(0)
+        .then(()=>console.log("promise callback1"))
+    
+    
+Promise.resolve()
     .then(()=>console.log('promise'))
-
-    Promise.resolve()
-        .then(()=>console.log('promise1'))
-        .then(()=>console.log('promise2'))
-
-        console.log('synch') //synchronous
