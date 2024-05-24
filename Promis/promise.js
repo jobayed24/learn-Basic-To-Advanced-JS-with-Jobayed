176d1009f68f63af184b9ec3c5b41e15ccc9026e
jobayed24 <jkhouse117@gmail.com>
2024-05-24 17:22:06 +0600

    Promise.all: fullfil the all promise before code execution

--- Promis/promise.js
+++ Promis/promise.js
@@ -1,13 +1,13 @@
-const wait =(ms)=>new Promise((res)=>setTimeout(res, ms))
+//Promise all
 
-wait(0)
-    .then(()=>console.log("promise callback"))
+const promise1=Promise.resolve(3);
 
-    const wait1 =(ms)=>new Promise((res)=>setTimeout(res, ms))
-
-    wait1(0)
-        .then(()=>console.log("promise callback1"))
-    
-    
-Promise.resolve()
-    .then(()=>console.log('promise'))
+const promise3=new Promise((resolve,reject)=>{
+    setTimeout(resolve, 100,'foo');
+})
+const promise2=new Promise((resolve)=> {
+    throw Error('error occured')
+});
+Promise.all([promise1,promise3,promise2,''])
+    .then((values)=>console.log(values))
+    .then(null,(reject)=>console.log(reject.message))
