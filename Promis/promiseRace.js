8581948d6dde78b1125126910f91359d7397c149
jobayed24 <jkhouse117@gmail.com>
2024-05-25 15:29:19 +0600

    Bkash api request using promise race

---
+++ Promis/promiseRace.js
@@ -0,0 +1,13 @@
+const data = Promise.race([
+    fetch("https://dummyjson.com/products/1"),
+    new Promise((resolve, reject) => {
+      // Reject after 5 seconds
+      setTimeout(() => reject(new Error("Request timed out")), 5000);
+    }),
+  ])
+    .then(async(res) => {
+      const data=await res.json();
+      console.log(data)
+    })
+    .catch((err) => console.log(err))
+    .finally(()=>console.log('from promise race'))
