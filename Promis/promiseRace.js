d3d84144116c2ef174f0ace8e8e767a04e89984d
jobayed24 <jkhouse117@gmail.com>
2024-05-25 19:39:19 +0600

    run promise after few milisecond

--- Promis/promiseRace.js
+++ Promis/promiseRace.js
@@ -1,13 +1,36 @@
-const data = Promise.race([
-    fetch("https://dummyjson.com/products/1"),
-    new Promise((resolve, reject) => {
-      // Reject after 5 seconds
-      setTimeout(() => reject(new Error("Request timed out")), 5000);
-    }),
-  ])
-    .then(async(res) => {
-      const data=await res.json();
-      console.log(data)
-    })
-    .catch((err) => console.log(err))
-    .finally(()=>console.log('from promise race'))
+
+const stateCondiion=(promise)=>{
+  // console.log(promise)
+  const pendingState={status: 'pending'}
+ return Promise.race([promise,pendingState])
+    .then((value)=>
+      // console.log(value)
+      value==pendingState ? value : {status: 'fulfiled',value}
+    ,(reason)=>
+      ({status: 'rejected',reason}))
+   
+}
+const p1=new Promise((resovle)=>setTimeout(resovle, 100,'promise1'))
+const p2=new Promise((resovle)=>setTimeout(resovle, 200,'promise2'))
+const p3=new Promise((resovle,reject)=>setTimeout(reject, 100,'promise3'))
+
+
+async function callFunction(){
+ console.log( await stateCondiion(p1))
+ console.log( await stateCondiion(p2))
+ console.log( await stateCondiion(p3))
+
+}
+
+console.log('initial state');
+callFunction();
+
+setTimeout(() => {
+  console.log('after 100ms')
+  callFunction()
+}, 100);
+
+setTimeout(() => {
+  console.log('after 200 ms')
+  callFunction()
+}, 200);
