d8eec730b173fa5920300037e8c28cd78952f728
jobayed24 <jkhouse117@gmail.com>
2024-08-01 23:23:45 +0600

    when return something from constructor function then the method and property will not executed

--- main.js
+++ main.js
@@ -1,58 +1,18 @@
-//root bluePrint of the class
 class vehicle{
-    constructor(brandName,typesOfEngine,dateOfProduction){
-        this.brandName=brandName;
-        this.typesOfEngine=typesOfEngine;
-        this.dateOfProduction=dateOfProduction;
-    }
-}
 
-class car extends vehicle{
-    // object or class property always set inside constructor function
-    constructor(brandName,typesOfEngine,dateOfProduction){
-        super(brandName,typesOfEngine,dateOfProduction)
-        this.color="Blue";
-        this.mileAge=43;
-        this.consumptionOfFeul='34'
-    }
-    
-}
-
-class truck extends vehicle{
-    constructor(brandName,typesOfEngine,dateOfProduction){
-        super(brandName,typesOfEngine,dateOfProduction)
-        this.length='15 meter';
-        this.goodWithWeight='189 kg';
-        this.wheels='4'
-    }
-
-    totalWheelsOfCar(){
-        const spareOfWheel=2;
-        const totalWheels=Number(spareOfWheel)+Number(this.wheels);
-        return totalWheels;
-    }
 }
 
 class van extends vehicle{
-    constructor(brandName,typesOfEngine,dateOfProduction){
-        super(brandName,typesOfEngine,dateOfProduction)
-        this.length='12 meter';
-        this.color= 'maroon';
-        this.isDrivingLicense= 'yeah, need driving license'
+    constructor(){
+        super()
+        return{
+            
+        }
     }
-}
-class bus extends vehicle{
-    constructor(brandName,typesOfEngine,dateOfProduction){
-        super(brandName,typesOfEngine,dateOfProduction)
-        this.totalSeat='78';
-        this.isHeavyLicense= 'yeah need havy license';
-        this.drivingHealthQuality= 'health quality should be well';
-        this.destination= 'destination is Cumilla to Dhaka'
+    wellCom(){
+        console.log('Welcom again to My journey')
     }
 }
 
-const truckObj= new truck('mercedes','diesel','1948');
-
-console.log(truckObj.totalWheelsOfCar())
-
-
+const vanObj=new van();
+vanObj.wellCom()
