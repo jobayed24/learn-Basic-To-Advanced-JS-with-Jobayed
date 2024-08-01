a160393ab69ed655f0854ad4ba0a44cc88a2ae07
jobayed24 <jkhouse117@gmail.com>
2024-08-01 23:12:40 +0600

    class or object instance create and access the property of the object

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
-function getNameAge(){
-    this.name=()=>{
-        return 'This is from Function';
-    };
-    this.age=45;
-    this.profession='doing coding';
-    return {
-        profession: 'doing coding but comes from obj',
-    };
+//root bluePrint of the class
+class vehicle{
+    constructor(brandName,typesOfEngine,dateOfProduction){
+        this.brandName=brandName;
+        this.typesOfEngine=typesOfEngine;
+        this.dateOfProduction=dateOfProduction;
+    }
 }
 
-const obj=new getNameAge();
+class car extends vehicle{
+    // object or class property always set inside constructor function
+    constructor(brandName,typesOfEngine,dateOfProduction){
+        super(brandName,typesOfEngine,dateOfProduction)
+        this.color="Blue";
+        this.mileAge=43;
+        this.consumptionOfFeul='34'
+    }
+    
+}
 
-console.log(obj.profession)
+class truck extends vehicle{
+    constructor(brandName,typesOfEngine,dateOfProduction){
+        super(brandName,typesOfEngine,dateOfProduction)
+        this.length='15 meter';
+        this.goodWithWeight='189 kg';
+        this.wheels='4'
+    }
+
+    totalWheelsOfCar(){
+        const spareOfWheel=2;
+        const totalWheels=Number(spareOfWheel)+Number(this.wheels);
+        return totalWheels;
+    }
+}
+
+class van extends vehicle{
+    constructor(brandName,typesOfEngine,dateOfProduction){
+        super(brandName,typesOfEngine,dateOfProduction)
+        this.length='12 meter';
+        this.color= 'maroon';
+        this.isDrivingLicense= 'yeah, need driving license'
+    }
+}
+class bus extends vehicle{
+    constructor(brandName,typesOfEngine,dateOfProduction){
+        super(brandName,typesOfEngine,dateOfProduction)
+        this.totalSeat='78';
+        this.isHeavyLicense= 'yeah need havy license';
+        this.drivingHealthQuality= 'health quality should be well';
+        this.destination= 'destination is Cumilla to Dhaka'
+    }
+}
+
+const truckObj= new truck('mercedes','diesel','1948');
+
+console.log(truckObj.totalWheelsOfCar())
+
 
