9b159aa3e7b26fbe5a0dab7d93d0d6ca880c5b34
jobayed24 <jkhouse117@gmail.com>
2024-08-02 11:50:56 +0600

    laxical scope: laxical scope same as clouser but it's executed sequentially

--- main.js
+++ main.js
@@ -3,15 +3,11 @@ function outerFunction(){
   const outerVariable='I am in outside !';
   function innerFunction(){
     console.log('enter innerFunction');
-    console.log(outerVariable);
-     
+    console.log(outerVariable);    
   }
   console.log('ending inner function')
-  return innerFunction;
+   innerFunction();
   console.log('ending outer function')
 }
 
-
-const cluser=outerFunction();
-console.log('completed outer function execution')
-cluser()
+outerFunction();
