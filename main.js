c36ad31c87c65d045276aaceb1910a1aac9c39f3
jobayed24 <jkhouse117@gmail.com>
2024-08-02 14:09:37 +0600

    appy method in function same as call but only difference is second arguments will array only

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ function add(c,d){
 
 const o={a: 1,b:2}
 
-const totalValue=add.call(o,10,20)
+const totalValue=add.apply(o,[10,20])// this with call method it's refer object which pass in firt argument and second argument it's expect explicit arguments with array which the function expect
 console.log(totalValue)
