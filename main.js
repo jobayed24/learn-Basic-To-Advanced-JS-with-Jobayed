2e59c9ebd273ced8fa414f4c9f5387f81865ddbe
jobayed24 <jkhouse117@gmail.com>
2024-08-03 10:14:01 +0600

    bound: it's bound the permanantly firstt arguments and create new function and body

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function add(c,d){
-  return this.a+this.b+c+d;
+function functionBody(){
+  return this.a;
 }
 
-const o={a: 1,b:2}
 
-const totalValue=add.apply(o,[10,20])// this with call method it's refer object which pass in firt argument and second argument it's expect explicit arguments with array which the function expect
-console.log(totalValue)
+const boundBody=functionBody.bind({a:'this is jobayed'});
+
+console.log(boundBody());
+
+const secondBoundBody=boundBody.bind({a: 'this is second function body'});
+
+console.log(secondBoundBody())
