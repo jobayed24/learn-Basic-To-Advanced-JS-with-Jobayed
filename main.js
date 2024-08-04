30ec0e2f23ec69c3266438b85093669e1d445af3
jobayed24 <jkhouse117@gmail.com>
2024-08-04 12:45:12 +0600

    object: object assign copy value from multiple source and assign it to target object with source value

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-function functionBody(){
-  return this.a;
-}
+const obj1={a:0,b:{c:0}};
+
+const obj2=Object.assign({},obj1);
+obj1.a=1;
+obj2.a = 2;
+obj1.b.c=4;
+obj2.b.c = 3;
 
 
-const boundBody=functionBody.bind({a:'this is jobayed'});
+console.log(obj1); // { a: 1, b: { c: 3 } }
+console.log(obj2); 
 
-console.log(boundBody());
-
-const secondBoundBody=boundBody.bind({a: 'this is second function body'});
-
-console.log(secondBoundBody())
+const obj3 = { a: 0, b: { c: 0 } };
+const obj4=structuredClone(obj3);
+obj3.a=3;
+obj4.b.c=4;
+console.log('obj3=',obj3,"obj4",obj4);
