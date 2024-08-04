fd6cfd8bde4c5bb0b7847382aa835143887a8c97
jobayed24 <jkhouse117@gmail.com>
2024-08-04 16:56:42 +0600

    object in prototype chain and non numberable property will not be copied

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-const obj1={a:0,b:{c:0}};
+const obj=Object.create(
+  {foo:1},
+  {
+    bar: {
+      value: 'bar'
+    },
+    baz:{
+      value: 'baz',
+      enumerable: true
+    }
+  },
+)
 
-const obj2=Object.assign({},obj1);
-obj1.a=1;
-obj2.a = 2;
-obj1.b.c=4;
-obj2.b.c = 3;
-
-
-console.log(obj1); // { a: 1, b: { c: 3 } }
-console.log(obj2); 
-
-const obj3 = { a: 0, b: { c: 0 } };
-const obj4=structuredClone(obj3);
-obj3.a=3;
-obj4.b.c=4;
-console.log('obj3=',obj3,"obj4",obj4);
+const obj1=Object.assign({},obj);
+console.log('obj',obj,'obj1',obj1)
