2fb01c655710509b91078dd071b250f88c1bc85e
jobayed24 <jkhouse117@gmail.com>
2024-08-05 06:51:56 +0600

    interrupt ongoing coping task

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const obj=Object.create(
-  {foo:1},
-  {
-    bar: {
-      value: 'bar'
-    },
-    baz:{
-      value: 'baz',
-      enumerable: true
-    }
-  },
-)
+// 'use strict'
+const obj=Object.defineProperty({},'foo',{
+    value: 1,
+    writable: false,
+})
+// obj.foo="2"
+// copy multiple object resource to obj object
+Object.assign(obj,{bar:2},{foo1:'foo1',foo2:'foo2',foo3: 'foo3'},{foo:'this is foo'},{foo6: 'foo6'});
+console.log(obj)
 
-const obj1=Object.assign({},obj);
-console.log('obj',obj,'obj1',obj1)
+//......Note.....
+
+//Copy task executed properly untile get error with assign value in read only property
