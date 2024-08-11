6ae1474ef1af4115350258f2082c6aeb650f7bb5
jobayed24 <jkhouse117@gmail.com>
2024-08-11 09:10:04 +0600

    Object.keys and getOwnPropertyName has used here

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-// null object which do not inherit properties
+//simple array
+const arr = ["a", "b", "c"];
+// find out the keys of array
+const arrayKeys=Object.keys(arr);
+console.log(arrayKeys)
+//array like object
+const arrayObj={
+  1: 'Orange',
+  2: 'Apple',
+  3: 'Pineapple',
+  4: 'Graps',
+};
 
-const obj=Object.create(null);
-obj.name='jobayed';
-console.log(Object.hasOwn(obj,'name'))
+// get-out the keys of arrayObj
+const arrayObjKeys=Object.values(arrayObj)
+console.log(arrayObjKeys);
+
+//non enumerable property
+const objEnumerable=Object.create({},{
+  getFoo:{
+    value(){
+      return this.foo;
+    },
+  }
+})
+
+objEnumerable.foo='foo is here';
+// object keys help to get the keys of non numerable property
+console.log(Object.keys(objEnumerable))
+
+for(const objKeys of Object.getOwnPropertyNames(objEnumerable)){
+  console.log(objKeys)
+}
+console.log(Object.getOwnPropertyNames(objEnumerable))
+console.log(Object.values('foo'))
