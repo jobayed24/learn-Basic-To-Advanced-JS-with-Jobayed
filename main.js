27b594d2f1f6cf1d261537e5f7159c02d3c77d22
jobayed24 <jkhouse117@gmail.com>
2024-08-16 09:31:50 +0600

    Object.entries

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-// when we assign value in prototype chain then it will store the value in prototype but when in same name we assign the value in object then it store in object not  prototype chain that's it...
-function MyName(){
-  MyName.prototype.fname='jobayed';
-  Object.defineProperty(MyName.prototype,'name',{
-    value: 'ohidur',
-    writable: true,
-  })
+// converting an object to a map
+// map constructor accept iteable of entries
+
+const obj = { foo: "bar", baz: 42 };
+const objMap=new Map(Object.entries(obj));
+console.log(objMap);
+
+//iterating through  a map
+// using array destructuring you can iterate through object easily
+
+const obj1 = { a: 5, b: 7, c: 9 };
+// using for of loop
+for(const [key,value] of Object.entries(obj1)){
+  console.log(value)
 }
 
-const obj1=new MyName();
-console.log(obj1)
-console.log(obj1.fname);
-obj1.fname='hossain';
-console.log(obj1.fname);
-console.log(obj1.name);
-obj1.name='tanvir';
-console.log(obj1.name)
+// using array method
+
+Object.entries(obj1).forEach(([key,value])=>{
+  console.log(`the key is ${key} and value is ${value}`)
+})
+
