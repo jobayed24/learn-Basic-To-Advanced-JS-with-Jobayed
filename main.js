94560d4197fbb35f5876f468f77b826970e25a5c
jobayed24 <jkhouse117@gmail.com>
2024-08-18 17:04:31 +0600

    Object.getOwnPropertyDescriptor clone the object by getOwnPropertyNames

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-const person = {
-  name: "Alice",
-  details: {
-    age: 30,
-    gender: "female"
-  }
+// so clone a object
+// structure clone or we can clone the object Object.assign
+
+const person={
+  name: 'jobayed',
+  designation: 'engineer',
+  occupation: {
+    rol: 'solve the problem'
+  },
 };
-// making function for deep freez..
-
-function deepFreez(object){
-  const personProperty=Object.getOwnPropertyNames(object);
-
-  for(const obProperty of personProperty){
-    const value=object[obProperty];
-
-    if(value && typeof value==='object'){
-      deepFreez(value)
-      
-    }
-  }
-  return Object.freeze(object)
+Object.defineProperty(person,'hobby',{
+  value: 'coding',
+  enumerable: false,
+});
+function cloneObject(object){
+  const obj={};
+  Object.getOwnPropertyNames(object).forEach((props)=>{
+    const pd=Object.getOwnPropertyDescriptor(object,props);
+    Object.defineProperty(obj,props,pd);
+   
+    // obj[props]=object[props];
+  });
+  return obj;
 }
+const personCopy=cloneObject(person);
+personCopy.occupation.rol='no jobs';
+personCopy.name='arif';
+person.name='jobayed'
+console.log('personCopy',personCopy);
+console.log('person',person);
 
-
-deepFreez(person);
-
-console.log(person.details)
+console.log(Object.getOwnPropertyDescriptors(personCopy))
