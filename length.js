51ab71bd92b7825b996a3328fef1ac534138a4a9
jobayed24 <jkhouse117@gmail.com>
2024-08-19 15:10:54 +0600

    hasOwn has the facilites to omit the property from object

--- length.js
+++
@@ -1,7 +0,0 @@
-// how to make any data to array
-const newArray=Function.prototype.call.bind(Array.prototype.slice)
-function list(){
-  return newArray(arguments)
-}
-
-console.log(list(1,2,3,4,{id: 1,rasel: "rasel"},5))
--- main.js
+++ main.js
@@ -1,32 +1,7 @@
-// so clone a object
-// structure clone or we can clone the object Object.assign
-
-const person={
-  name: 'jobayed',
-  designation: 'engineer',
-  occupation: {
-    rol: 'solve the problem'
-  },
-};
-Object.defineProperty(person,'hobby',{
-  value: 'coding',
-  enumerable: false,
-});
-function cloneObject(object){
-  const obj={};
-  Object.getOwnPropertyNames(object).forEach((props)=>{
-    const pd=Object.getOwnPropertyDescriptor(object,props);
-    Object.defineProperty(obj,props,pd);
-   
-    // obj[props]=object[props];
-  });
-  return obj;
+const example = { foo: true, bar: true };
+Object.prototype.name='jk'
+for(const lc in example){
+  if(Object.hasOwn(example,lc)){
+    console.log(lc)
+  }
 }
-const personCopy=cloneObject(person);
-personCopy.occupation.rol='no jobs';
-personCopy.name='arif';
-person.name='jobayed'
-console.log('personCopy',personCopy);
-console.log('person',person);
-
-console.log(Object.getOwnPropertyDescriptors(personCopy))
