1fdd592320beed9a327e9bb7b62edadfd4e2ba1c
jobayed24 <jkhouse117@gmail.com>
2024-08-21 04:07:13 +0600

    cms small project check content type using isPrototypeOf and run corespondent function

--- main.js
+++ main.js
@@ -1,16 +1,66 @@
-//Ensure compatibility and in Frameworks and liberies
+//small project related CMS
+// here we will make Article ,video ,images
 
-function Plugin(){};
+// base class 
+function ContentItem(title,author){
+    this.title=title;
+    this.author=author;
+};
 
-const plugin=new Plugin();
-function pluginRegistration(checkObj,mainObj){
-    if( mainObj.prototype.isPrototypeOf(checkObj)){
-        console.log('plugin registration successfully');
-    }else{
-        console.log('invalid plugin');
+// Article subClass
+function Article(title,author,content){
+    ContentItem.call(this,title,author)
+    this.content=content;
+}
+
+Article.prototype=Object.create(ContentItem.prototype);
+Article.prototype.constructor=Article
+
+//Video class
+function Video(title,author,duration){
+    ContentItem.call(this,title,author);
+    this.duration=duration;
+}
+
+Video.prototype=Object.create(ContentItem.prototype);
+Video.prototype.constructor=Video;
+
+//Image Class
+function Image(title,author,dimension){
+    ContentItem.call(this,title,author);
+    this.dimension=dimension;
+}
+
+Image.prototype=Object.create(ContentItem.prototype);
+Image.prototype.constructor=Image;
+// const contentItem=new ContentItem('test 1','jobayed');
+
+// create content instance
+
+let article = new Article("JavaScript Best Practices", "John Doe", "Lorem ipsum...");
+let video = new Video("Learn JavaScript", "Jane Smith", "15:30");
+let image = new Image("Beautiful Landscape", "Alice Johnson", "1920x1080");
+
+function processContent(content){
+    if(Article.prototype.isPrototypeOf(content)){
+        console.log('processing Article '+content.title);
+    }else if(Video.prototype.isPrototypeOf(content)){
+        console.log('processing Video '+content.title+"["+content.duration+"]");
+    }else if(Image.prototype.isPrototypeOf(content)){
+        console.log('processing Image '+content.title+"["+content.dimension+"]");
+    } else{
+        console.log('Unknown content type');
     }
 }
 
-const inheritedObj=Object.create(Plugin.prototype);
+processContent('article',article)
+processContent('video',video)
+processContent('image',image)
 
-pluginRegistration(inheritedObj,Plugin)
+// Extending functionality
+
+
+
+
+
+
