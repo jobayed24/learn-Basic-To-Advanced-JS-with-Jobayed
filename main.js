48d8df939f0f2f1a06349ac8160cfc3651005f5a
jobayed24 <jkhouse117@gmail.com>
2024-08-24 01:25:06 +0600

    we can't switch data property to acessor belongs to Object.seal()

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const element=document.getElementById('userForm').addEventListener('submit',(e)=>{
-    e.preventDefault();
-    // console.log(e)
-    const formData=new FormData(e.target);
-    // const fromentry=Object.fromEntries(formData);
-    const datas=Object.fromEntries(formData);
-    const convertDatas=Array.from(formData.entries());
-    const cData=Object.fromEntries(convertDatas);
-    // console.log('cData',cData)
-    console.log('datas',datas)
-   
-})
+const obj = {
+    prop() {},
+    foo: "bar",
+  };
+  Object.seal(obj);
+
+  obj.foo='foo';
+  function tellYourName(){
+    console.log('jk')
+  }
+  console.log(obj.foo)
+  Object.defineProperty(obj,'foo',{
+    get(){
+        return tellYourName;
+    }
+  })
