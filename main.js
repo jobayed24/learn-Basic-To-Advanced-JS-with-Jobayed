bf208ece32597888e6cfcc4a7aaec0f6d0d4e808
jobayed24 <jkhouse117@gmail.com>
2024-08-25 03:15:43 +0600

    object preventExtensions

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-const obj = {
-    prop() {},
-    foo: "bar",
-  };
-  Object.seal(obj);
+const object1 = {};
+object1.property1='value1';
+const getOwndes=Object.getOwnPropertyDescriptors(object1);
+console.log(getOwndes);
+Object.preventExtensions(object1);
+console.log('after',Object.getOwnPropertyDescriptors(object1));
+object1.name='jobayed';
+console.log(object1)
 
-  obj.foo='foo';
-  function tellYourName(){
-    console.log('jk')
-  }
-  console.log(obj.foo)
-  Object.defineProperty(obj,'foo',{
-    get(){
-        return tellYourName;
-    }
-  })
