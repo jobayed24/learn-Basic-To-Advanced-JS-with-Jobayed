09cab68a7ab1bf09c90335a3c6c701143c9a7ff6
jobayed24 <jkhouse117@gmail.com>
2024-08-25 21:06:49 +0600

    Object.valueOf() used for return primitive value in contrast to object

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const object1 = {};
-object1.property1='value1';
-const getOwndes=Object.getOwnPropertyDescriptors(object1);
-console.log(getOwndes);
-Object.preventExtensions(object1);
-console.log('after',Object.getOwnPropertyDescriptors(object1));
-object1.name='jobayed';
-console.log(object1)
+function RarellyNumber(number){
+  this.number=number
 
+}
+
+RarellyNumber.prototype.valueOf=function(){
+  return this.number;
+}
+
+const obj=new RarellyNumber(23);
+
+console.log(obj)
