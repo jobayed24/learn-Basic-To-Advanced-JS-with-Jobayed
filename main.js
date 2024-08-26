690f5558290cb56fc8f13a7e2a4fc1400fa1417e
jobayed24 <jkhouse117@gmail.com>
2024-08-26 14:21:16 +0600

    currency converter using [Symbol.toPritive]()--issue have default value not showed

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-// built money class
-class Money{
-    constructor(amount,currency){
+/// use-case of [Symbol.toPritive]()
+
+//assume we have class which used for ecommerce website to manipulate the input in different ways.
+
+class CurrencyFormatter{
+    constructor(amount,currency,location){
         this.amount=amount;
         this.currency=currency;
+        this.location=location;
     }
-    valueOf(){
-        return this.amount;
+
+    formateCurrency(){
+        const formatter= new Intl.NumberFormat(this.location,{
+            style:'currency',
+            currency: this.currency,
+        })
+        return  formatter.format(this.amount)
     }
-    toString(){
-        return `${this.currency}${this.amount}`
+
+    [Symbol.toPrimitive](hint){
+        switch(hint){
+            case 'string':
+                return this.formateCurrency();
+            case 'number':
+                return this.amount;
+            default:
+                return `${this.formateCurrency()} (${this.currency})`; // default value not worked
+        }
     }
 }
 
-const salary=new Money(700,'$');
-const bonous=new Money(300,'$');
-const total=salary+bonous;
-console.log(total);
-
-console.log(`Salary: ${salary}`);
-console.log(`Bonous: ${bonous}`)
+const usdPrice=new CurrencyFormatter(1000,'USD','en-US');
+const bd=new CurrencyFormatter(100,'SAR','SA');
+console.log(`${bd}`)
+console.log(usdPrice);
+console.log(`${usdPrice}`);
+console.log(usdPrice.toString())
