c0bb77fa6e62038bbc7916e6dc41e39f01bbc791
jobayed24 <jkhouse117@gmail.com>
2024-08-26 11:35:57 +0600

    object return primitive priority base before toSring call valueOf

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-function TellName(value){
-    this.value=value;
- function valueOf(){
-        return this.value;
+// built money class
+class Money{
+    constructor(amount,currency){
+        this.amount=amount;
+        this.currency=currency;
     }
- 
+    valueOf(){
+        return this.amount;
+    }
+    toString(){
+        return `${this.currency}${this.amount}`
+    }
 }
 
-const name=new TellName('jobayed');
+const salary=new Money(700,'$');
+const bonous=new Money(300,'$');
+const total=salary+bonous;
+console.log(total);
 
-console.log(name +'jk')
+console.log(`Salary: ${salary}`);
+console.log(`Bonous: ${bonous}`)
