6301f209814ad1e231a83e2ae2252bf1e4746e2e
jobayed24 <jkhouse117@gmail.com>
2024-08-29 06:57:23 +0600

    inherite parent prototype to child prototype \n \n in child constructor parent protoype should add should add child constructor

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
-// we will do calculate employee salaries
+// Parent constructor function
 
-const employeeSalaries = {
-    John: 50000,
-    Jane: 60000,
-    Doe: 45000,
-    Alice: 70000
+function Parent(name){
+   this.name=name;
+
+}
+
+Parent.prototype.parentMethod=function(){
+    console.log('parent method');
 };
+// create method return the constructor property of Parent
+Parent.prototype.create=function(){
+    // console.log('parentcons',new this.constructor);
+    return (new this.constructor);
+}
+// Child constructor
+function Child(name,age){
+    Parent.call(this,name);
+    this.age=age;
+};
+// Child method add in prototype
+Child.prototype.childMehtod=function(){
+    console.log('child method');
+}
+// create method return constructor of Child
+Child.prototype=Object.create(Parent.prototype,
+    // {
+    //     constructor:{
+    //         value: Child,
+    //         enumerable: false,
+    //         writeable: true,
+    //         configureable: true,
+    //     }
+    // }
+)
+Child.prototype.create=function(){
+    console.log(new this.constructor);
+    return (new this.constructor)
+}
+// calling parent mehtod
 
-const eachPersonSalary=Object.values(employeeSalaries);
-const totalSalary=eachPersonSalary.reduce((prev,curren)=>{
-    return prev+curren
-},0)
-// console.log(totalSalary)
-//Total salary
+const child=new Child('child',23);
+const parent=new Parent('parent')
 
-console.log(`Total employee salaries=`,totalSalary);
+console.log(child.create().create().create());
+// console.log('parent',parent)
+// console.log(child.age)
