495c5bfc149902d622df590ebe54e824a9137011
jobayed24 <jkhouse117@gmail.com>
2024-08-29 07:10:05 +0600

    Inherite parent using setPrototypeOf \n\n set child prototype parent constructor to child constructor

--- main.js
+++ main.js
@@ -1,47 +1,18 @@
-// Parent constructor function
+function Parent(){}
 
-function Parent(name){
-   this.name=name;
-
+Parent.prototype.callName=function(){
+    console.log(new this.constructor);
 }
 
-Parent.prototype.parentMethod=function(){
-    console.log('parent method');
-};
-// create method return the constructor property of Parent
-Parent.prototype.create=function(){
-    // console.log('parentcons',new this.constructor);
+function Child(){
+    Parent.call(this);
+}
+
+Child.prototype.callName=function(){
+    console.log(new this.constructor);
     return (new this.constructor);
 }
-// Child constructor
-function Child(name,age){
-    Parent.call(this,name);
-    this.age=age;
-};
-// Child method add in prototype
-Child.prototype.childMehtod=function(){
-    console.log('child method');
-}
-// create method return constructor of Child
-Child.prototype=Object.create(Parent.prototype,
-    // {
-    //     constructor:{
-    //         value: Child,
-    //         enumerable: false,
-    //         writeable: true,
-    //         configureable: true,
-    //     }
-    // }
-)
-Child.prototype.create=function(){
-    console.log(new this.constructor);
-    return (new this.constructor)
-}
-// calling parent mehtod
 
-const child=new Child('child',23);
-const parent=new Parent('parent')
+Object.setPrototypeOf(Child.prototype,Parent.prototype);
 
-console.log(child.create().create().create());
-// console.log('parent',parent)
-// console.log(child.age)
+new Child().callName().callName()
