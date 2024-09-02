3405771ceab0a8c15c466084296ed810c271f34b
jobayed24 <jkhouse117@gmail.com>
2024-09-02 19:36:05 +0600

    bind function
    unbound function make it bound with person object

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-// write a utility function with the goal of returning the array's lowest value and height.
+// bind function call the function explicitly
 
-function lowAndHeightValue(arr){
-    let minValue=Math.min.apply(null,arr)
-    let heightValue=Math.max.apply(null,arr);
-    return [minValue,heightValue];
-}
-const arr=[12,444,-10];
-const [min,height]=lowAndHeightValue(arr);
-console.log(min,height)
+const person={
+    name: 'jobayed',
+    role: 'developer',
+    age: 23,
+    getName:function(welcome,ourSite){
+        return `${welcome} ${this.name} at ${ourSite}`
+    }
+};
+
+const personName=person.getName.bind(person); // getName is standAlone function which bound the function with person object
+
+console.log(personName('Good After noon','my plateform'))
