8c2613c1770d07ae59223a05d0447f6dcb402c44
jobayed24 <jkhouse117@gmail.com>
2024-09-11 06:47:05 +0600

    symbol reference always reference unique

--- main.js
+++ main.js
@@ -1,68 +1,23 @@
-function validateForm(){
-    let isValid=true;
-    let name=document.getElementById('name').value;
-    let age=document.getElementById('age').value;
-    let email=document.getElementById('email').value;
+let userIdSymbol=Symbol('userId');
+let userPassSymbol=Symbol('userPass');
 
-    document.getElementById('successMessage').textContent='';
-    let nameInput= document.getElementById('name').textContent
-    let ageInput= document.getElementById('age').textContent
-    let emailInput= document.getElementById('email').textContent
-    nameInput='';
-    ageInput='';
-    emailInput='';
-    
-    if(name){
-        document.getElementById('nameError').textContent='';
-    }
-    if(age){
-        document.getElementById('ageError').textContent='';
-    }
-    if(email){
-        document.getElementById('emailError').textContent='';
-    }
-    if(!name){
-        document.getElementById('nameError').textContent='name is required';
-        isValid=false;
-    }
-    if(!age){
-        document.getElementById('ageError').textContent='age is required';
-        isValid=false;
-    }
-    if(!email){
-        document.getElementById('emailError').textContent='email is required';
-        isValid=false;
+class User{
+    constructor(name,userId,userPass){
+        this.name=name;
+        this[userIdSymbol]=userId;
+        this[userPassSymbol]=userPass;
     }
 
-    if(isValid){
-        document.getElementById('nameError').textContent=''
-        document.getElementById('ageError').textContent=''
-        document.getElementById('emailError').textContent=''
-        document.getElementById('successMessage').textContent='data submitted successfully';
+    getUserId(){
+        return this[userIdSymbol];
     }
-    
+    updatePass(newpass){
+        this[userPassSymbol]=newpass;
+    }
+    validateUserPass(newpass){
+        return this[userPassSymbol]==newpass;
+    }
 }
 
-function validateFormFieldOnChange(){
-    const name=document.getElementById('name').value;
-    const age=document.getElementById('age').value;
-    const email=document.getElementById('email').value;
-    if(name){
-        document.getElementById('nameError').textContent=''
-    }
-    if(age){
-        document.getElementById('ageError').textContent=''
-    }
-    if(email){
-        document.getElementById('emailError').textContent=''
-    }
-    if(!name){
-        document.getElementById('nameError').textContent='name is required'
-    }
-    if(!age){
-        document.getElementById('ageError').textContent='age is required'
-    }
-    if(!email){
-        document.getElementById('emailError').textContent='email is required'
-    }
-}
+const obj1=new User('jobayed','a1','a11');
+console.log(obj1.getUserId('a1'))
