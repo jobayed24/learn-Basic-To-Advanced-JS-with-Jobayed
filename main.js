4c17ad0985feb87ccde3ef53ab778305eb9c8ee9
jobayed24 <jkhouse117@gmail.com>
2024-09-11 13:29:28 +0600

    instance custom apply for array using symbol

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
-let userIdSymbol=Symbol('userId');
-let userPassSymbol=Symbol('userPass');
-
-class User{
-    constructor(name,userId,userPass){
-        this.name=name;
-        this[userIdSymbol]=userId;
-        this[userPassSymbol]=userPass;
-    }
-
-    getUserId(){
-        return this[userIdSymbol];
-    }
-    updatePass(newpass){
-        this[userPassSymbol]=newpass;
-    }
-    validateUserPass(newpass){
-        return this[userPassSymbol]==newpass;
+class Array1{
+    static [Symbol.hasInstance](instance){
+        return Array.isArray(instance)
     }
 }
 
-const obj1=new User('jobayed','a1','a11');
-console.log(obj1.getUserId('a1'))
+console.log([] instanceof Array1)
