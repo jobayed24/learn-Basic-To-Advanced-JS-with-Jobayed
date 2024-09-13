f0d79c65d16a77b02394416b480d2afda32dfe73
jobayed24 <jkhouse117@gmail.com>
2024-09-13 12:55:17 +0600

    there are three ways to get value using index

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const docUl=document.querySelector('ul');
-docUl.addEventListener('click',(ev)=>{
-    if(ev.target.tagName==='LI'){
-        ev.target.classList.toggle('done')
-    }
-},false)
+const cart = ["apple", "banana", "pear"];
+
+//return last element
+console.log(cart[cart.length-2])
+
+const getBanana=cart.slice(-2,-1);
+console.log(getBanana);
+
+console.log(cart.at(-2))
