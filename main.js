72e7d7406ae00bfa26efc65b7a2c8825d243498e
jobayed24 <jkhouse117@gmail.com>
2024-09-13 17:56:42 +0600

    concating non array like object using Array prototype.concat

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-const obj1 = { 0: 1, 1: 2, 2: 3, length: 3 };
-const obj2 = { 0: 1, 1: 2, 2: 3, length: 3, [Symbol.isConcatSpreadable]: true };
-console.log([0].concat(obj1,obj2))
+//concating non array object with concate
+
+console.log(Array.prototype.concat.call({},[1,2,3]))
+
+const arrayLike = {
+    [Symbol.isConcatSpreadable]: true,
+    length: 2,
+    0: 1,
+    1: 2,
+    2: 99, // ignored by concat() since length is 2
+  };
+
+  console.log(Array.prototype.concat.call(arrayLike,3,4))
