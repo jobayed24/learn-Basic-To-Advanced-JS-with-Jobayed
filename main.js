bd95fe64f3de7cff46ee9b036588f3666cb985f9
jobayed24 <jkhouse117@gmail.com>
2024-09-26 12:39:31 +0600

    small carousel project

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-//concating non array object with concate
+const products = ["Product1", "Product2", "Product3", "Product4", "Product5", "Product6"];
+let carouselView=products.slice(0,3);
+const carouselContainer=document.getElementById('carousel');
+const nextButton=document.getElementById('nextBtn');
+const prevButton=document.getElementById('prevBtn');
+function disPlayProducts(){
+    carouselContainer.innerText=''
+    carouselView.forEach((product)=>{
+        const div=document.createElement('div');
+        div.classList.add('items')
+        div.innerText=product;
+        carouselContainer.appendChild(div);
+    })
+    
+}
 
-console.log(Array.prototype.concat.call({},[1,2,3]))
+function nextProduct(){
+    let rememberPrevProduct=products[0];
+    products.shift();
+    carouselView=products.slice(0,3);
+    products.push(rememberPrevProduct);
+    disPlayProducts()
+    
+}
+function prevProduct(){
+    let rememberNextProduct=products[products.length-1];
+    console.log(rememberNextProduct)
+    products.pop();
+    products.unshift(rememberNextProduct)
+    carouselView=products.slice(0,3);
+    disPlayProducts()
+    
+}
+nextButton.addEventListener('click',nextProduct)
+prevButton.addEventListener('click',prevProduct)
 
-const arrayLike = {
-    [Symbol.isConcatSpreadable]: true,
-    length: 2,
-    0: 1,
-    1: 2,
-    2: 99, // ignored by concat() since length is 2
-  };
-
-  console.log(Array.prototype.concat.call(arrayLike,3,4))
+disPlayProducts();
